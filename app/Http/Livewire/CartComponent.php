76804a87b33d6ec13175bb8d5df1be6004997b39
Tarek Hasan <thasan@sanasoft.local>
2022-04-03 15:00:40 +0200

    qty

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -4,9 +4,22 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId){
+        $product=Cart::get($rowId);
+        $qty=$product->qty +1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function decriceQuantity($rowId){
+        $product=Cart::get($rowId);
+        $qty=$product->qty +1;
+        Cart::update($rowId,$qty);
+    }
+
     public function render()
     {
         $most_viewed_product=Product::inRandomOrder()->limit(8)->get();
